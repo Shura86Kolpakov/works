099fb35e059b787a33832a23535a19d7349798fd
Alexander <shura.kolpakov.86@inbox.ru>
2019-03-02 00:06:48 +0300

    создал work_3

---
+++ work_3.py
@@ -0,0 +1,25 @@
+﻿# easy1
+# def data():
+#     name = input('Введите Ваше имя:')
+#     age = input('Укажите Ваш возраст:')
+#     city = input('Введите Ваш город:')
+#     print('{}, {}год, проживает в городе {}'.format(name, age, city))
+# a = data()
+
+# easy2
+# import random
+#
+# nums = []
+# for _ in range(3):
+#     nums.append(random.randint(-100, 100))
+# print(nums)
+# print(max(nums))
+
+#easy3
+def func(**kwargs):
+    print(max(kwargs,key=''))
+
+
+
+
+
